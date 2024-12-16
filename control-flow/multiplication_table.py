972e543dbed3473ac65c8f123d144c9eaa0f10dc
Lesiba Seema <135166836+mbejestar@users.noreply.github.com>
2024-12-16 19:45:12 +0200

    Create multiplication_table.py

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,8 @@
+# multiplication_table.py  
+
+# Prompt User for a Number  
+number = int(input("Enter a number to see its multiplication table: "))  
+
+for i in range(1, 11):  
+    product = number * i  
+    print(f"{number} * {i} = {product}")
